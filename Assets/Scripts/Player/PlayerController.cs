2ad5036c677d0a1a6b7be3f6189030e3afbbb091
Xwilarg <xwilarg@protonmail.com>
2024-05-25 15:57:37 +0200

    Add basic controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DevsThatJam
 {
     public class PlayerController : MonoBehaviour
     {
+        private Rigidbody2D _rb;
+        private float _xMov;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = new(_xMov, _rb.velocity.y);
+        }
+
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            _xMov = value.ReadValue<Vector2>().x;
+            if (_xMov < 0f) _xMov = -1f;
+            else if (_xMov > 0f) _xMov = 1f;
+        }
     }
 }
