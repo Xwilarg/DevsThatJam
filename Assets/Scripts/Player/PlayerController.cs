f4b2e057f01d5e63bc09bc78e6c7df9a517835c9
Xwilarg <xwilarg@protonmail.com>
2024-05-26 01:22:31 +0200

    Add up/down for lift

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,11 +9,11 @@ namespace DevsThatJam
         private Transform _upLimit, _downLimit;
 
         [SerializeField]
-        private Transform _arm;
+        private Rigidbody2D _arm;
 
         private Rigidbody2D _rb;
         private float _xMov;
-        private float _yMox;
+        private float _yMov;
 
         private void Awake()
         {
@@ -23,6 +23,10 @@ namespace DevsThatJam
         private void FixedUpdate()
         {
             _rb.velocity = new(_xMov * 5f, _rb.velocity.y);
+            _arm.velocity = new(0f, _yMov);
+
+            if (_arm.transform.position.y < _downLimit.position.y) _arm.transform.position = new(_arm.transform.position.x, _downLimit.position.y);
+            if (_arm.transform.position.y > _upLimit.position.y) _arm.transform.position = new(_arm.transform.position.x, _upLimit.position.y);
         }
 
         private float BoundOne(float value)
@@ -34,7 +38,9 @@ namespace DevsThatJam
 
         public void OnMove(InputAction.CallbackContext value)
         {
-            _xMov = BoundOne(value.ReadValue<Vector2>().x);
+            var v2 = value.ReadValue<Vector2>();
+            _xMov = BoundOne(v2.x);
+            _yMov = BoundOne(v2.y);
         }
     }
 }
