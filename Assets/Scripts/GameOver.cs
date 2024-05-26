51bea486f194b580364633e483c2af1a0b3ce16c
Ryan <ry_wilmink@hotmail.com>
2024-05-26 14:25:14 -0400

    GameOver screen
    goes back to main menu

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,16 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Animator _anim;
+
+    float _inputDelay = 3f;
+
+    bool _canInput;
+
+    private void Awake()
     {
-        
+        _canInput = false;
+        StartCoroutine(InputDelayTimer());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_canInput)
+            if (Input.GetMouseButtonDown(0))
+                SceneManager.LoadScene("MainMenu");
+
+    }
+
+    IEnumerator InputDelayTimer()
+    {
+        yield return new WaitForSeconds(_inputDelay);
+        _canInput = true;
     }
 }
